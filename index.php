722485d33d1872c455ceefbf262f2517b44abd78
BTS info <BTS info@192.168.1.41>
2018-12-23 14:39:48 +0100

    1er commit du PPE

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+require_once 'includes/fct.inc.php';
+require_once 'includes/class.pdogsb.inc.php';
+session_start();
+$pdo = PdoGsb::getPdoGsb();
+$estConnecte = estConnecte();
+require 'vues/v_entete.php';
+$uc = filter_input(INPUT_GET, 'uc', FILTER_SANITIZE_STRING);
+if ($uc && ! $estConnecte) {
+    $uc = 'connexion';
+} elseif (empty($uc)) {
+    $uc = 'accueil';
+}
+switch ($uc) { // pr eviter de faire plrs test :switch et case c tt les test /possibilite
+case 'connexion':
+    include 'controleurs/c_connexion.php';
+    break;
+case 'accueil':
+    include 'controleurs/c_accueil.php';
+    break;
+case 'gererFrais':
+    include 'controleurs/c_gererFrais.php';
+    break;
+case 'etatFrais':
+    include 'controleurs/c_etatFrais.php';
+    break;
+case 'deconnexion':
+    include 'controleurs/c_deconnexion.php';
+    break;
+}
+require 'vues/v_pied.php';
